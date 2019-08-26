5c2a6762c3547f8252a5d4469b5621f70343b158
Практикант <practic@tagras.tatneft.ru>
2019-08-26 09:50:48 +0300

    AA

--- tt.py
+++ tt.py
@@ -1,8 +1,5 @@
 from git import Repo
 
 repo = Repo('C:\\Users\\practic\Desktop\\gg')
-origin = repo.remotes
-for i in origin:
-    print(i)
-    print(i.push(c))
-    print(1)
+origin = repo.remote(name='test')
+origin.push()
